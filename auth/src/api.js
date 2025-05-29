04b0b2131f76abe0c0174824ebfce678270f64d8
Rutuja Kagwade <rutujakagwade5@example.com>
2025-05-29 13:33:50 +0530

    Fix CORS to allow deployed frontend URL

--- auth/src/api.js
+++ auth/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://authentication-sau6.onrender.com/api",
   withCredentials: true, // to handle cookies if needed
 });
 
